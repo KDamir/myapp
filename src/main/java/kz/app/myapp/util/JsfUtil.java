650ead3c007339e0ae92e1a3c12d32add97c79ab
Damir <damir.keldibekov@gmail.com>
2014-12-30 23:26:47 +0400

    fix

--- src/main/java/kz/app/myapp/util/JsfUtil.java
+++ src/main/java/kz/app/myapp/util/JsfUtil.java
@@ -37,9 +37,9 @@ public class JsfUtil {
     }
 
     public static void addErrorMessages(List<String> messages) {
-        for (String message : messages) {
+        messages.stream().forEach((message) -> {
             addErrorMessage(message);
-        }
+        });
     }
 
     public static void addErrorMessage(String msg) {
